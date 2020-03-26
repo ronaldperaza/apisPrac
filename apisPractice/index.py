85fb7644c6ac0fd5ec2163a0a9bf21aca878df3f
ronaldperaza <ronaldperaza460@gmail.com>
2020-03-26 17:21:06 -0300

    change files

--- apisPractice/index.py
+++ apisPractice/index.py
@@ -1,14 +1,33 @@
 import requests
+import json
 
 if __name__== '__main__':
-    url = 'http://httpbin.org/get'
+    url = 'http://httpbin.org/post'
 
     args = { 'name': 'ronald', 'curso': 'python', 'level': 'mid', }
+
     response = requests.get(url, params=args)
+    print(response.url)
 
     if response.status_code == 200:
-        content = response.content
-        print(content)
-        # file = open('google.html', 'wb')
-        # file.write(content)
-        # file.close()
+        
+       response_json = response.json()
+       origin = response_json['origin']
+       print(origin)
+
+
+
+# import requests
+
+# if __name__== '__main__':
+#     url = 'http://httpbin.org/get'
+
+#     args = { 'name': 'ronald', 'curso': 'python', 'level': 'mid', }
+#     response = requests.get(url, params=args)
+
+#     if response.status_code == 200:
+#         content = response.content
+#         print(content)
+#         # file = open('google.html', 'wb')
+#         # file.write(content)
+#         # file.close()
