31d73f8a315ae5ee24f743d41763f93c306b2c8b
ronaldperaza <ronaldperaza460@gmail.com>
2020-03-26 11:06:15 -0300

    second commit

--- apisPractice/index.py
+++ apisPractice/index.py
@@ -1,11 +1,14 @@
 import requests
 
 if __name__== '__main__':
-    url = 'https://www.google.cl'
-    response = requests.get(url)
+    url = 'http://httpbin.org/get'
+
+    args = { 'name': 'ronald', 'curso': 'python', 'level': 'mid', }
+    response = requests.get(url, params=args)
 
     if response.status_code == 200:
         content = response.content
-        file = open('google.html', 'wb')
-        file.write(content)
-        file.close()
+        print(content)
+        # file = open('google.html', 'wb')
+        # file.write(content)
+        # file.close()
