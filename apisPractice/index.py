792d0ff0835a4f613d8c981d11ec3a8049a93e05
ronaldperaza <ronaldperaza460@gmail.com>
2020-03-26 10:47:52 -0300

    firts commit

---
+++ apisPractice/index.py
@@ -0,0 +1,11 @@
+import requests
+
+if __name__== '__main__':
+    url = 'https://www.google.cl'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.content
+        file = open('google.html', 'wb')
+        file.write(content)
+        file.close()
